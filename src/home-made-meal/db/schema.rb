1ff3f30546b34b4a3df55d6f6d257d07ab683041
saman-zdf <samanfyz68@gmail.com>
2021-07-23 10:13:19 +0800

    Profile model has been added

--- src/home-made-meal/db/schema.rb
+++ src/home-made-meal/db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_003400) do
+ActiveRecord::Schema.define(version: 2021_07_23_012922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.integer "street_number"
+    t.string "street_name"
+    t.string "suburb"
+    t.integer "postcode"
+    t.integer "state"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_07_23_003400) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
