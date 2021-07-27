fae685ae6bdfb8c1d7e9c094097b743fc586d955
saman-zdf <samanfyz68@gmail.com>
2021-07-27 14:34:38 +0800

    shopping cart has been added, user can add and remove from cart, user can see the subtotal of items price, might modify it later to have a quantity to choose

--- src/home-made-meal/app/controllers/application_controller.rb
+++ src/home-made-meal/app/controllers/application_controller.rb
@@ -1,18 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  before_action :load_cart
+  before_action :subtotal
 
-  # def after_sign_in_path_for(resource)
-  #   if resource.class == FoodItem
-  #     new_food_item_path
-  #   else
-  #     new_food_item_path || root_path
-  #   end
-  # end
+
   private 
+  # initializin the session for cart and it will be appended by product id 
+    def initialize_session
+      session[:cart] ||= []
+    end
+    # loadin out cart with the session with the value has passes
+    def load_cart
+      @cart = FoodItem.find(session[:cart])
+    end
     def after_sign_in_path_for(profile)
-      if current_user.profile 
+      if current_user.profile
         new_food_item_path
       else
         new_profile_path || root_path 
       end
+    end
+
+    def subtotal 
+      @subtotal = @cart.collect{|p| p.price.to_i}.sum
+      
     end
 end
