6becf994c7869c8924d2e77c35dd4bc269b3de22
saman-zdf <samanfyz68@gmail.com>
2021-08-09 08:27:51 +0800

    The about page has been added, also I changed some of the link in navbar, and give some styling to the forms

--- src/home-made-meal/app/controllers/home_controller.rb
+++ src/home-made-meal/app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   after_action :delete_cart
   def hero 
-    
   end
 
   def index
@@ -10,8 +9,10 @@ class HomeController < ApplicationController
     @q = FoodItem.ransack(params[:q])
     @food_items = @q.result(distinct: true)
   end
+
   def show
     @food_item = FoodItem.find(params[:id])
+    # after successful checkout assign the food_item.buyer_id to the current_user.profile.id, and also increment the sales count by one each time we have a successful checkout 
     if params[:checkout] == "success"
       @food_item.buyer_id = current_user.profile.id 
       @food_item.sales_quantity += 1
@@ -19,7 +20,11 @@ class HomeController < ApplicationController
     end
   end
 
+  def about
+  end
+
   private
+  # After successful checkout delete all the items in cart line items 
     def delete_cart
       if params[:checkout] == "success"
         @cart.line_items.delete_all
